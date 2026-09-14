d667d339a093d04c14a2521e1f963887e5e0a0d8
neoquan <tungquanwork@gmail>
2026-09-14 23:09:59 +0700

    add guidance for pq1_stack.

--- src/pq1_stack.rs
+++ src/pq1_stack.rs
@@ -1,5 +1,9 @@
 // https://leetcode.com/problems/build-an-array-with-stack-operations/description/?envType=problem-list-v2&envId=dsa-linear-shoal-stack
 
+// For each number i you read from the stream (1, 2, …, n): always Push it. 
+// Then, if i is not the number target currently wants, immediately Pop it back off.
+// If it is the number target wants, leave it on the stack.
+
 pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
     let mut result = Vec::new();
     let mut j = 0;
